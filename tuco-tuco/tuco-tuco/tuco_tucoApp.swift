bcab111c821e099cb542b2f54a4e10bdff4e127f
Arthur Lopes <arthurpsl99@gmail.com>
2024-02-26 13:43:43 -0300

    initial commit

---
+++ tuco-tuco/tuco-tuco/tuco_tucoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tuco_tucoApp.swift
+//  tuco-tuco
+//
+//  Created by Arthur Sobrosa on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct tuco_tucoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
